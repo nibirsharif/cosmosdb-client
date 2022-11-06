affacb4ccc31422bb7a3f63fc68cf139ee241915
Shariful Nibir <nibirsharif@gmail.com>
2022-11-06 18:09:52 -0500

    add server class and few packages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
+var cors = require("cors");
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -9,10 +11,34 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+  
+    const error = {
+      Message: err.message
+    }
+  
+    res.json(error);
+  });
 
 module.exports = app;
